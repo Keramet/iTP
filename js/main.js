d0cec261bc92108cb26c0c4fc665c122b168c0d1
EVK <keramet.kh@gmail.com>
2016-03-28 17:15:26 +0300

    next: bind onscroll to row & cell Header

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ var itp = itp || {};
 	
 	itp._config = {
 		rCount: 10,
-		cCount: 20
+		cCount: 20,
+		cell: { width: 100, height: 30 }	// px;
 	}
 
 	itp.init = function () {
@@ -28,35 +29,48 @@ var itp = itp || {};
 		//	tbody = table.getElementsByTagName('tbody'),
 			r, c, row;
 
-		for (r = 0; r < itp.rCount; r++) {
-			table.insertRow(r);
-			tableRow.insertRow(r).insertCell(0).textContent = r + 1;
+		if (itp._isCreat) { alert("Таблица уже создана!"); }
+		else {
 
-			for (c = 0; c < itp.cCount; c++) {
-				if (r === 0) { 
-					if (c === 0 ) { tableCol.insertRow(0) };
-					tableCol.rows[0].insertCell(c).textContent = String.fromCharCode(65 + c);
-				 }
-				table.rows[r].insertCell(c).textContent =  ( r === 0 ) ? c+1 :
-														   ( c === 0 ) ? r+1 : "";
+			table.width  = itp.cCount * itp._config.cell.width + "px";
+			table.height = itp.rCount * itp._config.cell.height + "px";
+
+
+			for (r = 0; r < itp.rCount; r++) {
+				table.insertRow(r);
+				tableRow.insertRow(r).insertCell(0).outerHTML = "<th>" + (r + 1) + "</th>";
+
+				for (c = 0; c < itp.cCount; c++) {
+					if (r === 0) { 
+						if (c === 0 ) { tableCol.insertRow(0) };
+						tableCol.rows[0].insertCell(c).outerHTML = "<th>" + String.fromCharCode(65 + c) + "</th>";
+						//tableCol.rows[0].insertCell(c).textContent = String.fromCharCode(65 + c);
+					 }
+					table.rows[r].insertCell(c);
+				//		table.rows[r].insertCell(c).textContent =  ( r === 0 ) ? c+1 :
+				//											   ( c === 0 ) ? r+1 : "";
+				}
 			}
-		}
 
-		table.onscroll = function() {
-  			var scrolledY = table.pageYOffset || document.documentElement.scrollTop,
-  				scrolledX = table.pageXOffset || document.documentElement.scrollLeft;
-  			console.log( "Y: " + scrolledY + 'px' );
-  			console.log( "X: " + scrolledX + 'px' );
-		}
+			itp._isCreat = true;
+
+			document.querySelector('main').style.display = "block";
+
+			table.onscroll = function() {
+				console.log("table.onscroll");
+  			//	var scrolledY = table.pageYOffset || document.documentElement.scrollTop,
+  		//			scrolledX = table.pageXOffset || document.documentElement.scrollLeft;
+  	//			console.log( "Y: " + scrolledY + 'px' );
+  //				console.log( "X: " + scrolledX + 'px' );
+			}
+			console.dir(table);
 
 	//	document.querySelector('.rowHeader').offsetHeight = document.querySelector('.table').offsetHeight;
 	//	console.log(document.querySelector('.table').style.height);
 	//	console.log(document.querySelector('.rowHeader').style);
 	//	console.log(table.offsetHeight);
 
-		console.dir(document.querySelector('hr'));
-
-		
+		}
 	}
 
 document.addEventListener("DOMContentLoaded", itp.init);
