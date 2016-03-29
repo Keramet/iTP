57e7e19ef6368a61d9b66a87ea9f0673365a6291
EVK <keramet.kh@gmail.com>
2016-03-29 18:51:36 +0300

    done: onscroll. need: fix bugs with onscroll

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var itp = itp || {};
 	itp._config = {
 		rCount: 10,
 		cCount: 20,
-		cell: { width: 100, height: 20 }	// px;
+		cell: { width: 100, height: 30 }	// px;
 	}
 
 	itp.init = function () {
@@ -31,36 +31,31 @@ var itp = itp || {};
 
 		if (itp._isCreat) { alert("Таблица уже создана!"); }
 		else {
-
-			table.width  = itp.cCount * itp._config.cell.width + "px";
-			table.height = itp.rCount * itp._config.cell.height + "px";
+			console.log( table.width  = itp.cCount * itp._config.cell.width + "px" );
+			console.log( table.height = itp.rCount * itp._config.cell.height + "px" );
+			tableCol.width = table.width;
+			tableRow.height = table.height;
 
 			table.addEventListener("click", function (e) {
 				var input; 
 
 				if (e.target.nodeName === "TD") {
+					e.target.className = "input";
 					input = document.createElement("input");
 					input.className = "inGrid";
 					input.value = e.target.innerHTML;
+
 					input.onblur = function () {
+				//		this.parentNode.classList.remove("input");
+						this.parentNode.class = "";
 						this.parentNode.innerHTML = this.value;
-					//	this.parentNode.removeChild(this);
 					};
-					e.target.style.padding = 0;
+
 					e.target.innerHTML = "";
 					e.target.appendChild(input);
 					input.focus();
-
-				//	console.log(e.target);
-				//	console.log(document.querySelector('td'));
-				//	console.log(e.target.nodeName);
 				}
-				
 			});
-
-
-
-
 
 			for (r = 0; r < itp.rCount; r++) {
 				table.insertRow(r);
@@ -73,25 +68,40 @@ var itp = itp || {};
 						//tableCol.rows[0].insertCell(c).textContent = String.fromCharCode(65 + c);
 					 }
 					table.rows[r].insertCell(c);
-				//		table.rows[r].insertCell(c).textContent =  ( r === 0 ) ? c+1 :
-				//											   ( c === 0 ) ? r+1 : "";
 				}
 			}
 
-			itp._isCreat = true;
+			itp._isCreat = true;	// 
 
 			document.querySelector('main').style.display = "block";
 
-			table.onscroll = function() {
+			var divTab = document.querySelector('.table');
+			divTab.onscroll = function() {
 				console.log("table.onscroll");
-  			//	var scrolledY = table.pageYOffset || document.documentElement.scrollTop,
-  		//			scrolledX = table.pageXOffset || document.documentElement.scrollLeft;
-  	//			console.log( "Y: " + scrolledY + 'px' );
-  //				console.log( "X: " + scrolledX + 'px' );
+  				var scrolledY = divTab.pageYOffset || divTab.scrollTop,
+  					scrolledX = divTab.pageXOffset || divTab.scrollLeft;
+  	
+  				if (scrolledY) { tableRow.style.top = -scrolledY + "px"; }
+
+  				if (scrolledX) {
+  					tableCol.style.left = -scrolledX + "px";
+  					console.log( "X: " + scrolledX + 'px' );
+  					console.log( tableCol.style.left);
+  				}
+  			//	console.log(divTab.pageYOffset);
+  			//	console.log(document.documentElement.scrollTop);
+			//	console.log(divTab.scrollTop);
+
+  			//	console.log( "X: " + scrolledX + 'px' );
 			}
-			console.dir(table);
-			console.dir(document.querySelector('td').height);
-			console.dir(document.querySelector('th').height);	
+		//	console.dir(table);
+		
+			console.log(tableCol.width);
+			console.log(tableCol.style.width);
+			console.log(document.querySelector('.colHeader').style.width);
+			console.log(document.querySelector('.colHeader').width);
+			console.log(table.width);
+		//	console.dir(document.querySelector('th').height);	
 	//	document.querySelector('.rowHeader').offsetHeight = document.querySelector('.table').offsetHeight;
 	//	console.log(document.querySelector('.table').style.height);
 	//	console.log(document.querySelector('.rowHeader').style);
