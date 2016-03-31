e486ad5f83d6e45df54b4780d32b19f8400462c6
EVK <keramet.kh@gmail.com>
2016-03-31 09:22:55 +0300

    done: dymanic add cells. need: fix itp._colName

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@ var itp = itp || {};
 		document.querySelector('#btnCreate').addEventListener("click", function () {
 			itp.rCount = rCountInput.value;
 			itp.cCount = cCountInput.value;
-			itp._createTables();
 			itp._createTabs();
 			itp._clickTabs();
+			itp._createTables();
 		});
 	}
 
@@ -34,7 +34,7 @@ var itp = itp || {};
 			tab.href = "#";
 			tab.innerHTML = "<span" + ( (!i) ? " class='active'" : "" ) + ">" + el + "</span>";
 			sheetsTab.appendChild(tab);
-			console.log(tab);
+		//	console.log(tab);
 		});
 	}
 
@@ -55,60 +55,44 @@ var itp = itp || {};
 	}
 		
 	itp._createTables = function () {
-		var table = document.querySelector('#itpTable'),
-			tableCol = document.querySelector('#itpTableCol'),
-			tableRow = document.querySelector('#itpTableRow'),
-			tbody = table.getElementsByTagName('tbody')[0],
-			r, c, row;
+		var r, c, scrlSize = 16;
+
+		itp.table = document.querySelector('#itpTable');
+		itp.tableCol = document.querySelector('#itpTableCol');
+		itp.tableRow = document.querySelector('#itpTableRow');
+		itp.tbody = itp.table.getElementsByTagName('tbody')[0];
 
 		if (itp._isCreate) { alert("Таблица уже создана!"); }
 		else {
-			table.width  = itp.cCount * itp._config.cell.width + "px";
-			table.height = itp.rCount * itp._config.cell.height + "px";
-			tableCol.width = table.width;
-			tableRow.height = table.height;
-
-			table.addEventListener("click", function (e) {
-				var input; 
-
-				if (e.target.nodeName === "TD") {
-					e.target.className = "input";
-					input = document.createElement("input");
-					input.className = "inGrid";
-					input.value = e.target.innerHTML;
-
-					input.onblur = function () {
-						this.parentNode.classList.remove("input");	//	можно так:	this.parentNode.class = "";
-						this.parentNode.innerHTML = this.value;
-					};
-
-					input.onkeyup = function (e) {
-						if (e.keyCode === 13) this.blur();
-					}
-
-					e.target.innerHTML = "";
-					e.target.appendChild(input);
-					input.focus();
-				}
-			});
+			itp.table.width  = itp.cCount * itp._config.cell.width + scrlSize + "px";
+			itp.table.height = itp.rCount * itp._config.cell.height + scrlSize + "px";
+			itp.tableCol.width = itp.table.width;
+			itp.tableRow.height = itp.table.height;
 
 			for (r = 0; r < itp.rCount; r++) {
-				tbody.insertRow(r);
-				tableRow.insertRow(r).insertCell(0).outerHTML = "<th>" + (r + 1) + "</th>";
+				itp.tbody.insertRow(r);
+				itp.tableRow.insertRow(r).insertCell(0).outerHTML = "<th>" + (r + 1) + "</th>";
 
 				for (c = 0; c < itp.cCount; c++) {
 					if (r === 0) { 
-						if (c === 0 ) { tableCol.insertRow(0) };
-						tableCol.rows[0].insertCell(c).outerHTML = "<th>" + String.fromCharCode(65 + c) + "</th>";
-						//tableCol.rows[0].insertCell(c).textContent = String.fromCharCode(65 + c);
+						if (c === 0 ) { itp.tableCol.insertRow(0) };
+						itp.tableCol.rows[0].insertCell(c).outerHTML = "<th>" + itp._colName(c) + "</th>";
 					 }
-					tbody.rows[r].insertCell(c);
+					itp.tbody.rows[r].insertCell(c);
 				}
 			}
 
+			itp.table.addEventListener("click", itp._clickGrid);
+
 			document.querySelector('.table').onscroll = function() {
-				tableRow.style.top  = -this.scrollTop  + "px";
-				tableCol.style.left = -this.scrollLeft + "px";
+				var needAddC = this.scrollWidth - (this.clientWidth + this.scrollLeft),
+					needAddR = this.scrollHeight - (this.clientHeight + this.scrollTop);
+
+				itp.tableRow.style.top  = -this.scrollTop  + "px";
+				itp.tableCol.style.left = -this.scrollLeft + "px";
+
+				if (needAddC < itp._config.cell.width) itp._addCol(1);
+				if (needAddR < itp._config.cell.height) itp._addRow(1);
 			}
 
 			itp._isCreate = true;	// отслеживает, была ли создана таблица
@@ -116,6 +100,68 @@ var itp = itp || {};
 
 		}
 	}
+
+	itp._clickGrid = function (e) {		// при нажатии на ячейку
+		var input; 
+
+		if (e.target.nodeName === "TD") {
+			e.target.className = "input";
+			input = document.createElement("input");
+			input.className = "inGrid";
+			input.value = e.target.innerHTML;
+
+			input.onblur = function () {
+				this.parentNode.classList.remove("input");	//	можно так:	this.parentNode.class = "";
+				this.parentNode.innerHTML = this.value;
+			};
+
+			input.onkeyup = function (e) {
+				if (e.keyCode === 13) this.blur();
+			}
+
+			e.target.innerHTML = "";
+			e.target.appendChild(input);
+			input.focus();
+		}
+	}
+
+	itp._addRow = function (n) {
+		var c;
+
+		itp.tbody.insertRow(itp.rCount);
+		itp.tableRow.insertRow(itp.rCount).insertCell(0).outerHTML = "<th>" + (+itp.rCount + 1) + "</th>";
+
+		for (c = 0; c < itp.cCount; c++) {
+			itp.tbody.rows[itp.rCount].insertCell(c);
+		}
+	
+		itp.rCount++;
+		itp.table.height = (itp.table.scrollHeight + itp._config.cell.height) + "px";
+		itp.tableRow.height = itp.table.height;
+	}
+
+	itp._addCol = function (n) {
+		var r;
+
+		itp.tableCol.rows[0].insertCell(itp.cCount).outerHTML = "<th>" + itp._colName(itp.cCount) + "</th>";
+
+		for (r = 0; r < itp.rCount; r++) {
+			itp.tbody.rows[r].insertCell(itp.cCount);
+		}
+
+		itp.cCount++;
+		itp.table.width = (itp.table.scrollWidth + itp._config.cell.width) + "px";
+		itp.tableCol.width = itp.table.width;	
+	}
+
+	itp._colName = function (n) {			//	!! Переделать !!
+		var str = String.fromCharCode(65 + n);
+
+		return str;
+	}
+
+
+
 
 document.addEventListener("DOMContentLoaded", itp.init);
 
